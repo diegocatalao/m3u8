2b56bb105717d3852afed7e743dabdc06179ddc8
Diego Catal <diegohutter@icloud.com>
2025-11-03 16:20:30 -0300

    refac: transform attr into the main parser

---
+++ src/parser.c
@@ -0,0 +1,151 @@
+#include "parser.h"
+
+#include <regex.h>
+#include <stdlib.h>
+
+m3u8_parser_status_t m3u8_parser_attr(char* buffer, m3u8_parser_attr_t* attrs) {
+  m3u8_parser_status_t status = M3U8_PARSER_STATUS_NO_ERROR;
+
+  regex_t    regex;
+  regmatch_t pmatch[3];  // 0: full match, 1: key, 2: value
+
+  char* cursor = buffer;
+  char* pattern = "([A-Z0-9_-]+)=(\"[^\"]*\"|[^,]+)";
+
+  memset(&regex, 0, sizeof(regex_t));
+
+  if (buffer == NULL) {
+    M3U8_RAISE(M3U8_PARSER_STATUS_INVALID_ARG, "Invalid arg buffer (null)");
+  }
+
+  if (attrs == NULL) {
+    M3U8_RAISE(M3U8_PARSER_STATUS_INVALID_ARG, "Invalid arg attrs (null)");
+  }
+
+  if (m3u8_linked_init(&attrs->list) != M3U8_LINKED_STATUS_NO_ERROR) {
+    M3U8_RAISE(M3U8_PARSER_STATUS_LIST_ERROR,
+               "Fail to initialize the attribute list");
+  }
+
+  if (regcomp(&regex, pattern, REG_EXTENDED) != 0) {
+    M3U8_RAISE(M3U8_PARSER_STATUS_MEM_ALLOC_ERROR, "Bad regular expression");
+  }
+
+  while (regexec(&regex, cursor, 3, pmatch, 0) == 0) {
+    m3u8_parser_attr_t* attr = malloc(sizeof(m3u8_parser_attr_t));
+
+    if (attr == NULL) {
+      M3U8_RAISE(M3U8_PARSER_STATUS_MEM_ALLOC_ERROR, "Unable to allocate attr");
+    }
+
+    memset(attr, 0, sizeof(m3u8_parser_attr_t));
+
+    size_t key_s = pmatch[1].rm_eo - pmatch[1].rm_so;
+    size_t value_s = pmatch[2].rm_eo - pmatch[2].rm_so;
+
+    attr->key = strndup(cursor + pmatch[1].rm_so, key_s);
+    attr->value = strndup(cursor + pmatch[2].rm_so, value_s);
+
+    if (!m3u8_linked_inb(&attrs->list, &attr->list)) {
+      M3U8_RAISE(M3U8_PARSER_STATUS_LIST_ERROR,
+                 "Could not insert attr in list");
+    }
+
+    if (pmatch[0].rm_eo == 0)
+      break;
+
+    cursor += pmatch[0].rm_eo;
+  }
+
+clean_up:
+  regfree(&regex);
+  return status;
+}
+
+m3u8_parser_status_t m3u8_parser_attr_from_key(m3u8_parser_attr_t*  attrs,
+                                               m3u8_parser_attr_t** attr,
+                                               char*                key) {
+  m3u8_parser_status_t status = M3U8_PARSER_STATUS_NO_ERROR;
+  m3u8_parser_attr_t*  entry = NULL;
+
+  if (attrs == NULL) {
+    M3U8_RAISE(M3U8_PARSER_STATUS_INVALID_ARG, "Invalid arg attrs (null)");
+  }
+
+  if (attr == NULL) {
+    M3U8_RAISE(M3U8_PARSER_STATUS_INVALID_ARG, "Invalid arg attr (null)");
+  }
+
+  if (key == NULL) {
+    M3U8_RAISE(M3U8_PARSER_STATUS_INVALID_ARG, "Invalid arg key (null)");
+  }
+
+  m3u8_linked_foreach(entry, &attrs->list, m3u8_parser_attr_t, list) {
+    if (strcmp(entry->key, key) == 0) {
+      *attr = entry;
+      return M3U8_PARSER_STATUS_NO_ERROR;
+    }
+  }
+
+  status = M3U8_PARSER_STATUS_NOT_FOUND;
+
+clean_up:
+  return status;
+}
+
+m3u8_parser_status_t m3u8_parser_attr_count(m3u8_parser_attr_t* attrs,
+                                            int*                size) {
+  int                  count = 0;
+  m3u8_parser_status_t status = M3U8_PARSER_STATUS_NO_ERROR;
+
+  if (attrs == NULL) {
+    M3U8_RAISE(M3U8_PARSER_STATUS_INVALID_ARG, "Invalid arg attrs (null)");
+  }
+
+  if (size == NULL) {
+    M3U8_RAISE(M3U8_PARSER_STATUS_INVALID_ARG, "Invalid arg size (null)");
+  }
+
+  m3u8_linked_count(&attrs->list, &count);  // NOTE: ignore any errors
+  *size = count;
+
+clean_up:
+  return status;
+}
+
+m3u8_parser_status_t m3u8_parser_attr_destroy(m3u8_parser_attr_t* attrs) {
+  m3u8_parser_status_t status = M3U8_PARSER_STATUS_NO_ERROR;
+
+  if (attrs == NULL) {
+    M3U8_RAISE(M3U8_PARSER_STATUS_INVALID_ARG, "Invalid argument attr (null)");
+  }
+
+  m3u8_linked_node_t* next = NULL;
+  m3u8_parser_attr_t* entry = NULL;
+  m3u8_linked_node_t* pivot = attrs->list.next;
+
+  while (pivot != &attrs->list) {
+    next = pivot->next;
+    entry = m3u8_linked_container_of(pivot, m3u8_parser_attr_t, list);
+
+    m3u8_linked_remove(&entry->list);
+
+    free(entry->key);
+    free(entry->value);
+    free(entry);
+
+    pivot = next;
+  }
+
+clean_up:
+  return status;
+}
+
+m3u8_parser_status_t m3u8_parser_from_str(const char*     buffer,
+                                          m3u8_parser_t** parser) {
+  m3u8_parser_status_t status = M3U8_PARSER_STATUS_NO_ERROR;
+}
+
+m3u8_parser_playlist_type_t m3u8_parser_playlist_type(const char* buffer) {
+  m3u8_parser_status_t status = M3U8_PARSER_STATUS_NO_ERROR;
+}
---
+++ src/parser.h
@@ -0,0 +1,168 @@
+#ifndef _M3U8_PARSER_H_
+#define _M3U8_PARSER_H_
+
+#include "linked.h"
+
+/**
+ * @enum m3u8_parser_status_t
+ * @brief Represents the status of a validation operation.
+ */
+typedef enum {
+  /** Operation completed successfully. */
+  M3U8_PARSER_STATUS_NO_ERROR,
+  /** A validation rule was not met. */
+  M3U8_PARSER_STATUS_FAILURE,
+  /** Invalid argument provided to a function. */
+  M3U8_PARSER_STATUS_INVALID_ARG,
+  /** Memory allocation failed. */
+  M3U8_PARSER_STATUS_MEM_ALLOC_ERROR,
+  /** An error occurred when trying to manipulate the list */
+  M3U8_PARSER_STATUS_LIST_ERROR,
+  /** Given a key from list, attribute was not found */
+  M3U8_PARSER_STATUS_NOT_FOUND,
+  /** An unknown or unspecified error occurred. */
+  M3U8_PARSER_STATUS_UNKNOWN_ERROR,
+} m3u8_parser_status_t;
+
+/**
+ * @enum m3u8_parser_tag_t
+ * @brief Represents all possible M3U8 tags identified by the parser.
+ */
+typedef enum {
+  /** Indicates the file is an M3U8 playlist. */
+  M3U8_PARSER_EXTM3U,
+  /** Specifies the playlist compatibility version. */
+  M3U8_PARSER_EXT_X_VERSION,
+  /** Samples can be decoded without other segments. */
+  M3U8_PARSER_EXT_X_INDEPENDENT_SEGMENTS,
+  /** Specifies a playlist of I-frames. */
+  M3U8_PARSER_EXT_X_I_FRAME_STREAM_INF,
+  /** Relates playlists with alternative renditions. */
+  M3U8_PARSER_EXT_X_MEDIA,
+  /** Allows arbitrary session data in a master playlist. */
+  M3U8_PARSER_EXT_X_SESSION_DATA,
+  /** Provides encryption keys for all media segments. */
+  M3U8_PARSER_EXT_X_SESSION_KEY,
+  /** Indicates a preferred starting point for a playlist. */
+  M3U8_PARSER_EXT_X_START,
+  /** Specifies a variant stream for adaptive playback. */
+  M3U8_PARSER_EXT_X_STREAM_INF,
+  /** Provides mutability info of the playlist (VOD/EVENT). */
+  M3U8_PARSER_EXT_X_PLAYLIST_TYPE,
+  /** Indicates that each segment describes a single I-frame. */
+  M3U8_PARSER_EXT_X_I_FRAMES_ONLY,
+  /** Specifies the maximum media segment duration. */
+  M3U8_PARSER_EXT_X_TARGETDURATION,
+  /** Sequence number of the first segment in a playlist. */
+  M3U8_PARSER_EXT_X_MEDIA_SEQUENCE,
+  /** Allows synchronization between different renditions. */
+  M3U8_PARSER_EXT_X_DISCONTINUITY_SEQUENCE,
+  /** Specifies the duration of a media segment. */
+  M3U8_PARSER_EXTINF,
+  /** Indicates a segment is a sub-range of its URI. */
+  M3U8_PARSER_EXT_X_BYTERANGE,
+  /** Indicates a discontinuity between segments. */
+  M3U8_PARSER_EXT_X_DISCONTINUITY,
+  /** Specifies how to decrypt media segments. */
+  M3U8_PARSER_EXT_X_KEY,
+  /** Specifies how to obtain the Media Initialization Section. */
+  M3U8_PARSER_EXT_X_MAP,
+  /** Associates a segment with an absolute date/time. */
+  M3U8_PARSER_EXT_X_PROGRAM_DATE_TIME,
+  /** Associates a date range with a set of attributes. */
+  M3U8_PARSER_EXT_X_DATERANGE,
+  /** Indicates no more segments will be added to the playlist. */
+  M3U8_PARSER_EXT_X_ENDLIST,
+} m3u8_parser_tag_t;
+
+/** @brief Represents the type of an M3U8 media. */
+typedef enum {
+  M3U8_PARSER_MASTER_PLAYLIST,  /**< Represents a master media */
+  M3U8_PARSER_MEDIA_PLAYLIST,   /**< Represents a media media */
+  M3U8_PARSER_UNKNOWN_PLAYLIST, /**< Represents a unknown media */
+} m3u8_parser_playlist_type_t;
+
+typedef struct {
+} m3u8_parser_t;
+
+/**
+ * @struct m3u8_parser_attr_t
+ * @brief Represents a single key-value attribute parsed from an M3U8 tag.
+ */
+typedef struct {
+  /** The attribute key (e.g., "BANDWIDTH"). */
+  char* key;
+  /** The attribute value (e.g., "1280000"). */
+  char* value;
+  /** Embedded node for circular linked list. */
+  m3u8_linked_node_t list;
+} m3u8_parser_attr_t;
+
+/**
+ * @brief Parses a buffer containing key-value attributes.
+ * @param buffer The null-terminated string containing tag attributes.
+ * @param attrs  Pointer to an @ref m3u8_parser_attr_t structure where parsed
+ *               attributes will be stored.
+ * @return @ref M3U8_ATTR_STATUS_NO_ERROR on success.
+ * @return @ref M3U8_ATTR_STATUS_INVALID_ARG if @p buffer or @p attrs is NULL.
+ * @return @ref M3U8_ATTR_STATUS_MEM_ALLOC_ERROR if memory allocation fails.
+ * @return @ref M3U8_ATTR_STATUS_REG_PATTERN_ERROR if regex compilation fails.
+ * @return @ref M3U8_ATTR_STATUS_LIST_ERROR if insertion into the list fails.
+ */
+m3u8_parser_status_t m3u8_parser_attr(char* buffer, m3u8_parser_attr_t* attrs);
+
+/**
+ * @brief Retrieves an attribute from the list by its key.
+ * @param attrs Pointer to the attribute list.
+ * @param attr  Double pointer to store the found attribute.
+ * @param key   The key of the attribute to find.
+ * @return @ref M3U8_ATTR_STATUS_NO_ERROR on success.
+ * @return @ref M3U8_ATTR_STATUS_NOT_FOUND if the key is not found.
+ * @return @ref M3U8_ATTR_STATUS_INVALID_ARG if @p attrs, @p attr, or @p key
+ *                                           is NULL.
+ */
+m3u8_parser_status_t m3u8_parser_attr_from_key(m3u8_parser_attr_t*  attrs,
+                                               m3u8_parser_attr_t** attr,
+                                               char*                key);
+
+/**
+ * @brief Counts the number of attributes in the list.
+ * @param attrs Pointer to the attribute list.
+ * @param size  Pointer to an integer where the count will be stored.
+ * @return @ref M3U8_ATTR_STATUS_NO_ERROR on success.
+ * @return @ref M3U8_ATTR_STATUS_INVALID_ARG if @p attrs or @p size is NULL.
+ */
+m3u8_parser_status_t m3u8_parser_attr_count(m3u8_parser_attr_t* attrs,
+                                            int*                size);
+
+/**
+ * @brief Frees all memory associated with the attribute list.
+ * @param attrs Pointer to the attribute list.
+ * @return @ref M3U8_ATTR_STATUS_NO_ERROR on success.
+ * @return @ref M3U8_ATTR_STATUS_INVALID_ARG if @p attrs is NULL.
+ */
+m3u8_parser_status_t m3u8_parser_attr_destroy(m3u8_parser_attr_t* attrs);
+
+/**
+ * @brief Parses an M3U8 string and initializes a parser object.
+ * @param buffer The M3U8 string buffer to be parsed.
+ * @param parser Double pointer to an @ref m3u8_parser_t object that will be
+ *               created.
+ * @return @ref M3U8_PARSER_STATUS_NO_ERROR on success.
+ * @return @ref M3U8_PARSER_STATUS_INVALID_ARG if @p buffer or @p parser is NULL.
+ * @return @ref M3U8_PARSER_STATUS_MEM_ALLOC_ERROR on memory allocation failure.
+ */
+m3u8_parser_status_t m3u8_parser_from_str(const char*     buffer,
+                                          m3u8_parser_t** parser);
+
+/**
+ * @brief Check if is a master or media media.
+ * @param buffer string of current master.
+ * @return @ref M3U8_MASTER_PLAYLIST a master media from buffer.
+ * @return @ref M3U8_MEDIA_PLAYLIST a media media from buffer.
+ * @return @ref M3U8_UNKNOWN_PLAYLIST a unknown media if no valid arguments are
+ *              received.
+ */
+m3u8_parser_playlist_type_t m3u8_parser_playlist_type(const char* buffer);
+
+#endif  // _M3U8_PARSER_H_
