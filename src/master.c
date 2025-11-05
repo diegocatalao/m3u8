6bc89592a1c4b620f15d9405e6f9356ab959cc23
Diego Catal <diegohutter@icloud.com>
2025-11-05 11:33:40 -0300

    feat: create a master lateral struct for master

--- src/master.c
+++ src/master.c
@@ -1,9 +1,25 @@
 #include "master.h"
 #include "logger.h"
 
-m3u8_master_status_t m3u8_master_create(master_t** master) {
+#include <stdlib.h>
+
   m3u8_master_status_t status = M3U8_MASTER_STATUS_NO_ERROR;
-  M3U8_RAISE(M3U8_MASTER_STATUS_UNKNOWN_ERROR, "Not implemented");
+
+  if (master == NULL) {
+    M3U8_RAISE(M3U8_MASTER_STATUS_INVALID_ARG, "Invalid arg master (null)");
+  }
+
+  if ((*master = (m3u8_master_t*)malloc(sizeof(m3u8_master_t))) == NULL) {
+    M3U8_RAISE(M3U8_MASTER_STATUS_MEM_ALLOC_ERROR,
+               "Failed to allocate m3u8_master_t");
+  }
+
+  memset(*master, 0, sizeof(m3u8_master_t));
+
+  /** set the initial value of this struct */
+  (*master)->is_signed = false;
+  (*master)->version = 1;
+
 clean_up:
   return status;
 }
