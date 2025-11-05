6cd4fdb759e6290287fd64208426584a6a25c828
Diego Catal <diegohutter@icloud.com>
2025-11-04 22:58:01 -0300

    feat: set the validation stubs

---
+++ src/validate.c
@@ -0,0 +1,163 @@
+#include "validate.h"
+
+m3u8_validate_status_t m3u8_validate_extm3u(master_t* master) {
+  m3u8_validate_status_t status = M3U8_VALIDATE_STATUS_NO_ERROR;
+  RAISE(M3U8_VALIDATE_STATUS_UNKNOWN_ERROR, "Not implemented");
+clean_up:
+  return status;
+}
+
+m3u8_validate_status_t m3u8_validate_ext_x_version(master_t* master) {
+  m3u8_validate_status_t status = M3U8_VALIDATE_STATUS_NO_ERROR;
+  RAISE(M3U8_VALIDATE_STATUS_UNKNOWN_ERROR, "Not implemented");
+clean_up:
+  return status;
+}
+
+m3u8_validate_status_t m3u8_validate_ext_x_independent_segments(
+  master_t* master) {
+  m3u8_validate_status_t status = M3U8_VALIDATE_STATUS_NO_ERROR;
+  RAISE(M3U8_VALIDATE_STATUS_UNKNOWN_ERROR, "Not implemented");
+clean_up:
+  return status;
+}
+
+m3u8_validate_status_t m3u8_validate_ext_x_start(ext_x_start_t* ext_x_start) {
+  m3u8_validate_status_t status = M3U8_VALIDATE_STATUS_NO_ERROR;
+  RAISE(M3U8_VALIDATE_STATUS_UNKNOWN_ERROR, "Not implemented");
+clean_up:
+  return status;
+}
+
+m3u8_validate_status_t m3u8_validate_ext_x_session_data(
+  ext_x_session_data_t* session_data) {
+  m3u8_validate_status_t status = M3U8_VALIDATE_STATUS_NO_ERROR;
+  RAISE(M3U8_VALIDATE_STATUS_UNKNOWN_ERROR, "Not implemented");
+clean_up:
+  return status;
+}
+
+m3u8_validate_status_t m3u8_validate_ext_x_session_key(
+  ext_x_session_key_t* session_key) {
+  m3u8_validate_status_t status = M3U8_VALIDATE_STATUS_NO_ERROR;
+  RAISE(M3U8_VALIDATE_STATUS_UNKNOWN_ERROR, "Not implemented");
+clean_up:
+  return status;
+}
+
+m3u8_validate_status_t m3u8_validate_ext_x_media(ext_x_media_t* media) {
+  m3u8_validate_status_t status = M3U8_VALIDATE_STATUS_NO_ERROR;
+  RAISE(M3U8_VALIDATE_STATUS_UNKNOWN_ERROR, "Not implemented");
+clean_up:
+  return status;
+}
+
+m3u8_validate_status_t m3u8_validate_ext_x_stream_inf(
+  ext_x_stream_inf_t* stream_inf) {
+  m3u8_validate_status_t status = M3U8_VALIDATE_STATUS_NO_ERROR;
+  RAISE(M3U8_VALIDATE_STATUS_UNKNOWN_ERROR, "Not implemented");
+clean_up:
+  return status;
+}
+
+m3u8_validate_status_t m3u8_validate_ext_x_i_frame_stream_inf(
+  ext_x_i_frame_stream_inf_t* i_frame_stream_inf) {
+  m3u8_validate_status_t status = M3U8_VALIDATE_STATUS_NO_ERROR;
+  RAISE(M3U8_VALIDATE_STATUS_UNKNOWN_ERROR, "Not implemented");
+clean_up:
+  return status;
+}
+
+m3u8_validate_status_t m3u8_validate_ext_x_playlist_type(media_t* media) {
+  m3u8_validate_status_t status = M3U8_VALIDATE_STATUS_NO_ERROR;
+  RAISE(M3U8_VALIDATE_STATUS_UNKNOWN_ERROR, "Not implemented");
+clean_up:
+  return status;
+}
+
+m3u8_validate_status_t m3u8_validate_ext_x_i_frames_only(media_t* media) {
+  m3u8_validate_status_t status = M3U8_VALIDATE_STATUS_NO_ERROR;
+  RAISE(M3U8_VALIDATE_STATUS_UNKNOWN_ERROR, "Not implemented");
+clean_up:
+  return status;
+}
+
+m3u8_validate_status_t m3u8_validate_ext_x_targetduration(media_t* media) {
+  m3u8_validate_status_t status = M3U8_VALIDATE_STATUS_NO_ERROR;
+  RAISE(M3U8_VALIDATE_STATUS_UNKNOWN_ERROR, "Not implemented");
+clean_up:
+  return status;
+}
+
+m3u8_validate_status_t m3u8_validate_ext_x_media_sequence(media_t* media) {
+  m3u8_validate_status_t status = M3U8_VALIDATE_STATUS_NO_ERROR;
+  RAISE(M3U8_VALIDATE_STATUS_UNKNOWN_ERROR, "Not implemented");
+clean_up:
+  return status;
+}
+
+m3u8_validate_status_t m3u8_validate_ext_x_discontinuity_sequence(
+  media_t* media) {
+  m3u8_validate_status_t status = M3U8_VALIDATE_STATUS_NO_ERROR;
+  RAISE(M3U8_VALIDATE_STATUS_UNKNOWN_ERROR, "Not implemented");
+clean_up:
+  return status;
+}
+
+m3u8_validate_status_t m3u8_validate_extinf(ext_x_extinf_t* extinf) {
+  m3u8_validate_status_t status = M3U8_VALIDATE_STATUS_NO_ERROR;
+  RAISE(M3U8_VALIDATE_STATUS_UNKNOWN_ERROR, "Not implemented");
+clean_up:
+  return status;
+}
+
+m3u8_validate_status_t m3u8_validate_ext_x_byterange(
+  ext_x_byterange_t* byterange) {
+  m3u8_validate_status_t status = M3U8_VALIDATE_STATUS_NO_ERROR;
+  RAISE(M3U8_VALIDATE_STATUS_UNKNOWN_ERROR, "Not implemented");
+clean_up:
+  return status;
+}
+
+m3u8_validate_status_t m3u8_validate_ext_x_discontinuity(media_t* media) {
+  m3u8_validate_status_t status = M3U8_VALIDATE_STATUS_NO_ERROR;
+  RAISE(M3U8_VALIDATE_STATUS_UNKNOWN_ERROR, "Not implemented");
+clean_up:
+  return status;
+}
+
+m3u8_validate_status_t m3u8_validate_ext_x_key(ext_x_key_t* key) {
+  m3u8_validate_status_t status = M3U8_VALIDATE_STATUS_NO_ERROR;
+  RAISE(M3U8_VALIDATE_STATUS_UNKNOWN_ERROR, "Not implemented");
+clean_up:
+  return status;
+}
+
+m3u8_validate_status_t m3u8_validate_ext_x_map(ext_x_map_t* map) {
+  m3u8_validate_status_t status = M3U8_VALIDATE_STATUS_NO_ERROR;
+  RAISE(M3U8_VALIDATE_STATUS_UNKNOWN_ERROR, "Not implemented");
+clean_up:
+  return status;
+}
+
+m3u8_validate_status_t m3u8_validate_ext_x_program_date_time(media_t* media) {
+  m3u8_validate_status_t status = M3U8_VALIDATE_STATUS_NO_ERROR;
+  RAISE(M3U8_VALIDATE_STATUS_UNKNOWN_ERROR, "Not implemented");
+clean_up:
+  return status;
+}
+
+m3u8_validate_status_t m3u8_validate_ext_x_daterange(
+  ext_x_daterange_t* daterange) {
+  m3u8_validate_status_t status = M3U8_VALIDATE_STATUS_NO_ERROR;
+  RAISE(M3U8_VALIDATE_STATUS_UNKNOWN_ERROR, "Not implemented");
+clean_up:
+  return status;
+}
+
+m3u8_validate_status_t m3u8_validate_ext_x_endlist(media_t* media) {
+  m3u8_validate_status_t status = M3U8_VALIDATE_STATUS_NO_ERROR;
+  RAISE(M3U8_VALIDATE_STATUS_UNKNOWN_ERROR, "Not implemented");
+clean_up:
+  return status;
+}
