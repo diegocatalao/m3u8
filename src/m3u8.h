2957295f28ab7864cda4b7b0c28acb8f4e2c200d
Diego Catal <diegohutter@icloud.com>
2025-11-07 15:41:54 -0300

    chore: adjust documentation pattern

--- src/m3u8.h
+++ src/m3u8.h
@@ -34,8 +34,10 @@ typedef enum {
 
 /** @brief Represents an M3U8 media, which can be a master or media. */
 typedef struct {
-  m3u8_master_t* master; /** Master playlist with differents video formats */
-  m3u8_media_t*  media;  /** Media playlist with differents types and formats */
+  /** Master playlist with differents video formats */
+  m3u8_master_t* master;
+  /** Media playlist with differents types and formats */
+  m3u8_media_t* media;
 } m3u8_t;
 
 /**
