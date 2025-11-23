8d45c7c09d21933110a28acc74eeba922e4b12f7
Diego Catal <diegohutter@icloud.com>
2025-11-23 18:14:02 -0300

    feat: memory management without corruption

--- src/memory.h
+++ src/memory.h
@@ -6,21 +6,21 @@
 
 #include <stdlib.h>
 
-#define M3U8_MALLOC(ptr, size)                                              \
-  do {                                                                      \
-    if ((ptr = calloc(1, size)) == NULL) {                                  \
-      M3U8_RAISE(M3U8_STATUS_MEM_ALLOC_ERROR, "Failed to allocate memory"); \
-    }                                                                       \
+#define M3U8_MALLOC(ptr, type, size)                                             \
+  do {                                                                           \
+    if ((ptr = (type*)calloc(1, size)) == NULL) {                                \
+      M3U8_RAISE((int)M3U8_STATUS_MEM_ALLOC_ERROR, "Failed to allocate memory"); \
+    }                                                                            \
   } while (0)
 
-#define M3U8_CHECK_DEALLOC(ptr)                                                        \
-  do {                                                                                 \
-    if ((ptr) == NULL) {                                                               \
-      M3U8_RAISE(M3U8_STATUS_MEM_ALLOC_ERROR, "Attempt to deallocate a NULL pointer"); \
-    } else {                                                                           \
-      free(ptr);                                                                       \
-      ptr = NULL;                                                                      \
-    }                                                                                  \
+#define M3U8_CHECK_DEALLOC(ptr)                                                             \
+  do {                                                                                      \
+    if ((ptr) == NULL) {                                                                    \
+      M3U8_RAISE((int)M3U8_STATUS_MEM_ALLOC_ERROR, "Attempt to deallocate a NULL pointer"); \
+    } else {                                                                                \
+      free(ptr);                                                                            \
+      ptr = NULL;                                                                           \
+    }                                                                                       \
   } while (0)
 
 #endif  // _M3U8_MEMORY_H_
